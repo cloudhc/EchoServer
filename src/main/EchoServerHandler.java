e815f6f13df53842c4f2b4b88c86f3926443f5c8
bast <cloudhc@naver.com>
2016-04-18 13:30:17 +0900

    Initial commit

---
+++ src/main/EchoServerHandler.java
@@ -0,0 +1,24 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by bast on 2016-04-18.
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String readMessage = ((ByteBuf)msg).toString(Charset.defaultCharset());
+
+        System.out.println("수신한 문자열 [" + readMessage + "]");
+
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
